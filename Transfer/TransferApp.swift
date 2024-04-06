7307d8823a57e2f6dee8d253540c29e5bf52ebdc
Pranav R <pranav81234@gmail.com>
2024-04-06 15:30:29 +0530

    Initial Commit

---
+++ Transfer/TransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferApp.swift
+//  Transfer
+//
+//  Created by Pranav R on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
